d393924c1d1a9a3ab87867365bea13686fcbe738
Genadij Razdorov <genadijrazdorov@gmail.com>
2020-06-26 14:43:49 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pyabif',
 
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
 
     description='A Python ABIF reader',
